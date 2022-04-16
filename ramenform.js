0bfdd830423bc2c9313e8195ff7afb50831ed7a6
Mr0scar1 <kennyscrub762@gmail.com>
2022-04-16 17:07:33 +0800

    onblur

--- ramenform.js
+++ ramenform.js
@@ -9,11 +9,11 @@ const errorNo=document.querySelector('.error-number')
 const errorAddress=document.querySelector('.error-address')
 const errorEmail=document.querySelector('.error-email')
 //re is the regex to check it have not "@"
-const re= /.+[^@].+/;
+const re= /.+[@].+/;
 
 
 
-//add evenlistener to check the submit button
+//add evenlistener to check the information when the user click submit button
 form.addEventListener('submit', (s) => {
     // if  the form of contact name didn't type anything
     // it will generate an error message under the box of contact name
@@ -53,7 +53,6 @@ form.addEventListener('submit', (s) => {
     }
     // if the form of address didn't type anything
     // it will generate an error message under the box of address
-
     let messagesAdd= []
     if(address.value === '' || address.value == null) {
         messagesAdd.push('*Address cannot be blank')
@@ -66,7 +65,7 @@ form.addEventListener('submit', (s) => {
         s.preventDefault()
         errorAddress.innerText = messagesAdd.join(' ')
     }
-    // if the form of address didn't type anything and
+    // if the form of email didn't type anything and
     // didn't input the element of @
     // it will generate an error message under the box of address
     let messagesEmail=[]
@@ -77,6 +76,7 @@ form.addEventListener('submit', (s) => {
     if(email.value != null){
         messagesEmail.push('')
     }
+    //check email format xxx@xxx
     const ok=re.exec(email.value)
     if(!ok){
         messagesEmail.push('Email is invalid')
@@ -85,8 +85,87 @@ form.addEventListener('submit', (s) => {
         s.preventDefault()
         errorEmail.innerText = messagesEmail.join(' ')
     }
-    console.log(ok)
+})
 
+//onblur to check the input information
+name.addEventListener('blur', (e) =>{
+    // if  the form of contact name didn't type anything
+    // it will generate an error message under the box of contact name
+    let messagesName = []
+    if (name.value === '' || name.value == null) {
+        messagesName.push('*Contact Name cannot be blank')
+    }
+    //if user reinput the information, it will clear the error message
+    if(name.value != null){
+        messagesName.push('')
+    }
+    if (messagesName.length > 0) {
+        e.preventDefault()
+        errorName.innerText = messagesName.join(' ')
+    }
 
+})
 
+number.addEventListener('blur', (e) =>{
+    // if the form of contact number didn't type anything and
+    //didn't match the length of number equal to 8
+    // it will generate an error message under the box of contact name
+    let messagesNo = []
+    if (number.value.length > 0) {
+        if (number.value.length < 8 || number.value.length > 8) {
+            messagesNo.push('*Contact Number is invalid')
+        }
+    }
+    //if user reinput the information, it will clear the error message
+    if(number.value != null){
+        messagesNo.push('')
+    }
+    if(number.value === '' || number.value == null) {
+        messagesNo.push('*Contact Number cannot be blank')
+    }
+
+    if (messagesNo.length >0){
+        e.preventDefault()
+        errorNo.innerText = messagesNo.join(' ')
+    }
+})
+
+address.addEventListener('blur', (e) =>{
+    // if the form of address didn't type anything
+    // it will generate an error message under the box of address
+    let messagesAdd= []
+    if(address.value === '' || address.value == null) {
+        messagesAdd.push('*Address cannot be blank')
+    }
+    //if user reinput the information, it will clear the error message
+    if(address.value != null){
+        messagesAdd.push('')
+    }
+    if (messagesAdd.length >0){
+        e.preventDefault()
+        errorAddress.innerText = messagesAdd.join(' ')
+    }
+})
+
+email.addEventListener('blur', (e) =>{
+    // if the form of email didn't type anything and
+    // didn't input the element of @
+    // it will generate an error message under the box of address
+    let messagesEmail=[]
+    if(email.value === '' || email.value == null) {
+        messagesEmail.push('*Email cannot be blank')
+    }
+    //if user reinput the information, it will clear the error message
+    if(email.value != null){
+        messagesEmail.push('')
+    }
+    //check email format xxx@xxx
+    const ok=re.exec(email.value)
+    if(!ok){
+        messagesEmail.push('Email is invalid')
+    }
+    if (messagesEmail.length >0){
+        e.preventDefault()
+        errorEmail.innerText = messagesEmail.join(' ')
+    }
 })
